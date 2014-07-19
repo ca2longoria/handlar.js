32b9704e1b7d2d5858e027d588906d6ec399b074
ca2longoria <ca2longoria@gmail.com>
2014-07-19 00:36:09 +0000

    #1 Refactored, WOOHOO; makeHandle makes a little more sense, inside, though I still get confused reading through it over and over.  I need to make a per-case flowchart.

--- handlar.js
+++ handlar.js
@@ -68,6 +68,47 @@ Model = (function()
 		makeHandle(this,ob);
 	}
 	
+	// NOTE: callEvent is intended as an internal parameter.  The publicly
+	//   visible $delete method will *always* call its topmost event listener
+	//   functions.
+	function deleteHandle(h,callEvent,eventRecurse,ind)
+	{
+		ind = (!ind ? 0 : ind);
+		console.log('deleteHandle.'+ind+':',h,h.$,callEvent,eventRecurse);
+		// Following its first call, deeper recursions will take eventRecurse
+		// as this boolean check value.
+		if (callEvent)
+		{
+			listeners[h.__id].delete.map(function(action)
+			{ action.func(h.$) });
+			
+			listeners[h.__id].all.map(function(action)
+			{ action.func('delete',[h.$]) });
+		}
+		
+		var todel = [];
+		Object.getOwnPropertyNames(h).map(function(x)
+		{
+			if (h[x] instanceof Handle && h[x] !== h.__parent)
+				// Recursion, here.
+				deleteHandle(h[x],eventRecurse,(callEvent&&eventRecurse),ind+1);
+			else
+				todel.push(x);
+		});
+		
+		delete listeners[h.__id];
+		
+		if (h.__parent)
+		{
+			h.__parent.__expose();
+			delete h.__parent[h.__property];
+			delete h.__parent.__ob[h.__property];
+			h.__parent.__conceal();
+		}
+		
+		todel.map(function(x){ delete h[x]; });
+	}
+	
 	// NOTE: I want these Handle objects to remain *independent* of Model
 	//   instances, though...  Will think through, later.
 	function makeHandle(handle,ob)
@@ -91,7 +132,7 @@ Model = (function()
 				writable:false,
 				value:modelId+'_'+_.uniqueId()
 			});
-		
+			
 			// Add listeners entry, with arrays of event callbacks.
 			//
 			// NOTE: In the event that the listeners object is moved to a private,
@@ -114,21 +155,75 @@ Model = (function()
 				value:function(eventName,func)
 				{ return offEvent(self,eventName,func); }
 			});
+			
 		}
 		
+		// NOTE: __expose and __conceal are HACKS.  These shouldn't be necessary,
+		//   but it turns out the ob[k] get override requires the *parent's* local
+		//   'ob' variable, too.  At least for the delete.  This is happening,
+		//   because I chose for $delete to be called from the Handle being
+		//   deleted, and not its parent, deleting its child.  I may choose to
+		//   revise this, later.
+		Object.defineProperty(handle,'__expose',{
+			enumerable:false,
+			configurable:true,
+			writable:false,
+			value:function()
+			{
+				this.__ob = ob;
+			}
+		});
+		
+		Object.defineProperty(handle,'__conceal',{
+			enumerable:false,
+			configurable:true,
+			writable:false,
+			value:function()
+			{
+				delete this.__ob;
+			}
+		});
+	
 		// Assign basic JSON-object export function.
 		Object.defineProperty(handle,'$',{
 			enumerable:false,
 			configurable:true,
 			get:function()
 			{
+				//console.log('$   $:',ob);
 				if (typeof ob !== 'object')
 					return ob;
 				
 				var ret = (Array.isArray(ob) ? [] : {});
 				for (var p in ob)
 					ret[p] = ob[p].$;
+				//console.log('the $:',ret);
 				return ret;
+			}
+		});
+		
+		Object.defineProperty(handle,'$delete',{
+			enumerable:false,
+			configurable:true,
+			writable:false,
+			value:function(eventRecurse)
+			{
+				eventRecurse = (typeof eventRecurse !== 'undefined' ?
+					eventRecurse : false);
+				deleteHandle(self,true,eventRecurse);
+			}
+		});
+		
+		Object.defineProperty(handle,'$modify',{
+			enumerable:false,
+			configurable:true,
+			writable:false,
+			value:function(b)
+			{
+				for (var p in b)
+				{
+					
+				}
 			}
 		});
 		
@@ -141,190 +236,340 @@ Model = (function()
 			Object.defineProperty(handle,'valueOf',{
 				enumerable:false,
 				configurable:true,
-				value:function()
-				{ console.log('val?',ob); return ob; }
+				value:function(){ console.log('val?',ob); return ob; }
+			});
+			
+			// NOTE: Wait, do I need to define these?  Aren't they inherited from
+			//   Object type?  A simple assign should do, should it not?
+			Object.defineProperty(handle,'toString',{
+				enumerable:false,
+				configurable:true,
+				value:function(){ console.log('str?',ob); return ob.toString(); }
 			});
 		}
 		
 		if (typeof ob === 'object')
-		for (var p in ob)
 		{
-			//ob[p] = (typeof ob[p] === 'object' ? new Handle(ob[p]) : ob[p]);
-			//ob[p] = new Handle(ob[p]);
+			// Modify all.
 			
-			console.log('ob[p]',[ob,p,ob[p],ob[p].__id]);
-			// NOTE: If going for soft overwrites, this, too, needs to be a call to
-			//   makeHandle, rather than new Handle(...).  Unless this is only ever
-			//   accessed at creation, and not on assign.  One moment...
-			if (handle[p] instanceof Handle)
-				ob[p] = makeHandle(handle[p],ob[p]);
-			else
-				ob[p] = new Handle(ob[p]);
+			// NOTE: Hmm.....  Regarding *modify all*, there are several cases:
+			// 	 1) handle[p] exists, and is being [changed]
+			// 	 2) handle[p] does *not* exist, and is being created
+			// 	 3) handle[p] exists, but ob[p] does *not*, and so it is [deleted]
 			
-			if (handle[p] != ob[p])
+			var hKeys = Object.keys(handle);
+			var oKeys = Object.keys(ob).filter(
+				function(k){ return typeof ob[k] !== 'undefined' });
+			
+			var hnoto = _.difference(hKeys,oKeys);
+			var hando = _.intersection(hKeys,oKeys);
+			var onoth = _.difference(oKeys,hKeys);
+			
+			//console.log('bah:',handle,ob);
+			//console.log('these two:',hKeys,oKeys);
+			//console.log('these three:',hnoto,hando,onoth);
+			
+			// Deleting... <- hnoto
+			hnoto.map(function(p)
 			{
+				var h = handle[p];
+				console.log('deleting:',h,h.$);
+				
+				deleteHandle(h,true,true);
+			});
+			
+			// Changing... <- hando
+			hando.map(function(p)
+			{
+				// NOTE: REMEMBER, in each of these cases, hnoto, hando, and onoth,
+				//   *both* handle[p] *and* the local ob[p] must be modified.
+				
+				var oldValue = handle[p].$;
+				
+				makeHandle(handle[p],ob[p]);
+				ob[p] = handle[p];
+				
+				var newValue = handle[p].$;
+				
+				//console.log('changing changed:',oldValue,newValue);
+				
+				// Run 'change' event listeners.
+				listeners[handle[p].__id].change.map(function(action)
+				{
+					action.func(newValue,oldValue,action.args);
+				});
+			
+				// Run 'all' event listeners.
+				listeners[handle[p].__id].all.map(function(action)
+				{
+					action.func('change',[newValue,oldValue,action.args]);
+				});
+					
+			});
+			
+			// Adding... <- onoth
+			onoth.map(function(p)
+			{
+				ob[p] = new Handle(ob[p]);
+				
 				Object.defineProperty(ob[p],'__parent',{
 					enumerable:false,
 					configurable:false,
 					get:function(){return handle}
 				});
-			
-				// NOTE: Put this here for private access to its parent while
-				//   retaining its given property name.
-				
-				Object.defineProperty(ob[p],'$delete',{
+				Object.defineProperty(ob[p],'__property',{
 					enumerable:false,
 					configurable:false,
-					value:(function(k){return function(){ self[k] = special.delete }})(p)
+					writable:false,
+					value:p
 				});
+		
+				Object.defineProperty(handle,p,{
+					enumerable:true,
+					configurable:true,
+					get:(function(k){return function(){return ob[k]}})(p),
+					set:(function(k){return function(val)
+					{ handleSet(val,k); }})(p)
+				});
+			});
+			
+			function handleSet(val,k)
+			{
+				var oldHandle = ob[k];
+				var newHandle;
+				//console.log('handleSet()',val,k,oldHandle);
+				
+				// Store old value before possible modification, if it will have a
+				// use, later on.
+				var oldValue;
+				if (listeners[oldHandle.__id].change.length > 0 ||
+						listeners[oldHandle.__id].all.length > 0)
+					oldValue = oldHandle.$;
+				
+				// NOTE: Should 'change' only be called if the value has, in fact,
+				//   changed?
+				
+				// Assign newHandle.
+				if (val instanceof Handle)
+				{
+					// Replace.
+					//newHandle = val;
+					throw {
+						name:'BlockingThisForNowException',
+						message:'For now, no replacement.'
+					};
+				}
+				else if (typeof val === 'object')
+				{
+					newHandle = makeHandle(oldHandle,val);
+				}
+				else
+				{
+					newHandle = makeHandle(oldHandle,val);						
+				}
+				
+				// Adopt oldHandle's listeners.
+				if (newHandle !== oldHandle)
+				{
+					_.extend(listeners[newHandle.__id],listeners[oldHandle.__id]);
+					delete listeners[oldHandle.__id];
+				}
+				
+				// Run 'change' event listeners.
+				listeners[newHandle.__id].change.map(function(action)
+				{
+					action.func(val,oldValue,action.args);
+				});
+			
+				// Run 'all' event listeners.
+				listeners[newHandle.__id].all.map(function(action)
+				{
+					action.func('change',[val,oldValue,action.args]);
+				});
+				
+				//console.log('ob['+k+'] =',newHandle);
+				ob[k] = newHandle;
+				
+				return newHandle;
 			}
 			
-			Object.defineProperty(handle,p,{
-				enumerable:true,
-				configurable:true,
-				get:(function(k){return function(){return ob[k]}})(p),
-				set:(function(k){return function(val)
+			// NOTE: Disable this, for the moment, while I refactor.
+			if (false)
+			for (var p in ob)
+			{
+				//ob[p] = (typeof ob[p] === 'object' ? new Handle(ob[p]) : ob[p]);
+				//ob[p] = new Handle(ob[p]);
+				
+				console.log('ob[p]',[ob,p,ob[p],ob[p].__id]);
+				
+				
+				// NOTE: If going for soft overwrites, this, too, needs to be a call to
+				//   makeHandle, rather than new Handle(...).  Unless this is only ever
+				//   accessed at creation, and not on assign.  One moment...
+				if (handle[p] instanceof Handle)
 				{
-					var oldHandle = ob[k];
-					var newHandle;
-					
-					if (val == special.delete)
+					ob[p] = makeHandle(handle[p],ob[p]);
+				}
+				else
+					ob[p] = new Handle(ob[p]);
+				
+				if (handle[p] != ob[p])
+				{
+					Object.defineProperty(ob[p],'__parent',{
+						enumerable:false,
+						configurable:false,
+						get:function(){return handle}
+					});
+					Object.defineProperty(ob[p],'__property',{
+						enumerable:false,
+						configurable:false,
+						writable:false,
+						value:p
+					});
+				
+					// NOTE: Put this here for private access to its parent while
+					//   retaining its given property name.
+					Object.defineProperty(ob[p],'$delete',{
+						enumerable:false,
+						configurable:false,
+						// NOTE: eventRecurse calls delete event for all descendants.
+						value:(function(k,eventRecurse)
+						{return function(){ self[k] = special.delete }})(p)
+					});
+				}
+				
+				Object.defineProperty(handle,p,{
+					enumerable:true,
+					configurable:true,
+					get:(function(k){return function(){return ob[k]}})(p),
+					set:(function(k){return function(val)
 					{
-						// NOTE: Deletion needs to go somewhere more accissible.  Now that
-						//   there's a proper $delete method, this ought go somewhere more
-						//   sensible.
+						var oldHandle = ob[k];
+						var newHandle;
 						
-						listeners[oldHandle.__id].delete.map(function(action)
+						if (val == special.delete)
 						{
-							action.func(oldHandle.$);
-						});
-						
-						listeners[oldHandle.__id].all.map(function(action)
-						{
-							action.func('delete',[oldHandle.$]);
-						});
-						
-						// NOTE: WAAAAIT!
-						//   This must recurse.  It needs to recurse.
-						//   Or does it?  What if something else is using these?
-						//
-						//   You know what?  This is all based on the idea of a unified
-						//   object model, meaning there is only *one* root object for
-						//   each of these.  Modifying a Handle *should* invalidate all
-						//   the Handles below it. 
-						//*
-						for (var x in oldHandle)
-							if (oldHandle[x] instanceof Handle)
-								oldHandle[x] = special.delete;
-						//*/
-						
-						// NOTE: There is another concern, however.  In the event that a
-						//   Handle is assigned another object structure, creating more
-						//   handles, ought paths/values matching between the recursed
-						//   deleted old and the new created Handle be applied to the new,
-						//   as well?
-						//
-						//   By values, I mean things held by private Model vars,
-						//   like the listeners object.
-						//
-						//   Ultimately, that depends on whether the object is being
-						//   *replaced* or *modified*.  How might we distinguish between
-						//   the two?
-						//
-						//   Maybe... replace = new M.Hanlde({}), modify = {}.
-						//   Or visa-versa.  Could work either way.
-						//
-						//   Save this for a later commit.  Or better yet, make a github
-						//   Issue.
-						
-						// Remove events from listeners object.
-						delete listeners[oldHandle.__id];
-						delete ob[k];
-						delete self[k];
-						return;
-					}
-					
-					// Get old value only if it will be necessary later on in calling
-					// listeners.
-					var oldValue;
-					if (listeners[oldHandle.__id].change.length > 0 ||
-					    listeners[oldHandle.__id].all.length > 0)
-						oldValue = oldHandle.$;
-					
-					// NOTE: Replace-vs-Modify logic will have to change this, here.
-					//newHandle = (val instanceof Handle ? val : new Handle(val));
-					if (val instanceof Handle)
-					{
-						// Replace.
-						//newHandle = val;
-						throw {
-							name:'BlockingThisForNowException',
-							message:'For now, no replacement.'
-						};
-					}
-					else if (typeof val === 'object')
-					{
-						// Modify.
-						//newHandle = new Handle(val);
-						newHandle = makeHandle(oldHandle,val);
-						
-						/* If not replacing oldHandle, the external data need not
-						   be removed.
-						   
-						// Recursively transfer listeners.
-						// 
-						// NOTE: Seems to work, but on thinking over it, again, perhaps
-						//   passing in a regular object should traverse that regular
-						//   object *alone*, and modify oldHandle *without* replacing it,
-						//   entirely.  I'll think it over.  For now, it effectively does
-						//   already what I made it to do, so all's good.
-						pairWalk(oldHandle,newHandle,function(old,newh,key,path)
-						{
-							console.log('hokay!',key,old[key],newh[key]);
+							// NOTE: Deletion needs to go somewhere more accissible.  Now that
+							//   there's a proper $delete method, this ought go somewhere more
+							//   sensible.
+							console.log('deleting:',oldHandle.$);
 							
-							if (newh[key])
-								_.extend(listeners[newh[key].__id],listeners[old[key].__id]);
-							delete listeners[old[key].__id];
-						});
-						//*/
-					}
-					else
-						//newHandle = new Handle(val);
-						newHandle = makeHandle(oldHandle,val);
-					
-					// Modify listeners object.
-					// - Copy oldHandle's listeners over to newHandle's.
-					// - Remove oldHandle's listeners. 
-					if (newHandle !== oldHandle)
-					{
-						_.extend(listeners[newHandle.__id],listeners[oldHandle.__id]);
-						delete listeners[oldHandle.__id];
-					}
-					
-					ob[k] = newHandle;
-					
-					if (oldValue)
-					{
-						// Run change event listeners.
-						listeners[ob[k].__id].change.map(function(action)
+							listeners[oldHandle.__id].delete.map(function(action)
+							{
+								action.func(oldHandle.$);
+							});
+							
+							listeners[oldHandle.__id].all.map(function(action)
+							{
+								action.func('delete',[oldHandle.$]);
+							});
+							
+							/*
+							for (var x in oldHandle)
+								if (oldHandle[x] instanceof Handle)
+									oldHandle[x] = special.delete;
+							//*/
+							//*
+							Object.getOwnPropertyNames(oldHandle).map(function(x)
+							{
+								if (oldHandle[x] instanceof Handle)
+									oldHandle[x] = special.delete;
+								else
+									delete oldHandle[x];
+							});
+							//*/
+							
+							// Remove events from listeners object.
+							delete listeners[oldHandle.__id];
+							delete ob[k];
+							delete self[k];
+							return;
+						}
+						
+						// Get old value only if it will be necessary later on in calling
+						// listeners.
+						var oldValue;
+						if (listeners[oldHandle.__id].change.length > 0 ||
+								listeners[oldHandle.__id].all.length > 0)
+							oldValue = oldHandle.$;
+						
+						// NOTE: Replace-vs-Modify logic will have to change this, here.
+						//newHandle = (val instanceof Handle ? val : new Handle(val));
+						if (val instanceof Handle)
 						{
-							action.func(val,oldValue,action.args);
-						});
-					
-						// Run 'all' event listeners.
-						listeners[ob[k].__id].all.map(function(action)
+							// Replace.
+							//newHandle = val;
+							throw {
+								name:'BlockingThisForNowException',
+								message:'For now, no replacement.'
+							};
+						}
+						else if (typeof val === 'object')
 						{
-							action.func('change',[val,oldValue,action.args]);
-						});
-					}
-					
-					// Should events also bubble up...?
-					
-					console.log('returning',ob[k]);
-					return ob[k];
-				}})(p)
-			});
-		}
+							// Modify.
+							//newHandle = new Handle(val);
+							newHandle = makeHandle(oldHandle,val);
+							
+							/* If not replacing oldHandle, the external data need not
+								 be removed.
+								 
+							// Recursively transfer listeners.
+							// 
+							// NOTE: Seems to work, but on thinking over it, again, perhaps
+							//   passing in a regular object should traverse that regular
+							//   object *alone*, and modify oldHandle *without* replacing it,
+							//   entirely.  I'll think it over.  For now, it effectively does
+							//   already what I made it to do, so all's good.
+							pairWalk(oldHandle,newHandle,function(old,newh,key,path)
+							{
+								console.log('hokay!',key,old[key],newh[key]);
+								
+								if (newh[key])
+									_.extend(listeners[newh[key].__id],listeners[old[key].__id]);
+								delete listeners[old[key].__id];
+							});
+							//*/
+						}
+						else
+							//newHandle = new Handle(val);
+							newHandle = makeHandle(oldHandle,val);
+						
+						// Modify listeners object.
+						// - Copy oldHandle's listeners over to newHandle's.
+						// - Remove oldHandle's listeners. 
+						if (newHandle !== oldHandle)
+						{
+							_.extend(listeners[newHandle.__id],listeners[oldHandle.__id]);
+							delete listeners[oldHandle.__id];
+						}
+						
+						ob[k] = newHandle;
+						
+						if (oldValue)
+						{
+							// Run change event listeners.
+							listeners[ob[k].__id].change.map(function(action)
+							{
+								action.func(val,oldValue,action.args);
+							});
+						
+							// Run 'all' event listeners.
+							listeners[ob[k].__id].all.map(function(action)
+							{
+								action.func('change',[val,oldValue,action.args]);
+							});
+						}
+						
+						// Should events also bubble up...?
+						
+						console.log('returning',ob[k]);
+						return ob[k];
+					}})(p)
+				});
+			}
+		}	
+		
+		//console.log('final ob:',ob);
 		
 		return handle;
 	}
